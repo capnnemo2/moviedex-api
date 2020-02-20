97540c8dad585dcb6923be207f31f1bb3622c949
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 07:57:39 -0800

    Adds helmet

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
 const moviedex = require("./moviedex.json");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
