654622600e8bcdf15c997efb6b058a8c5a34e7da
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 07:42:12 -0800

    Adds req query filters

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const morgan = require("morgan");
+const moviedex = require("./moviedex.json");
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello");
+app.get("/movie", function handleGetMovie(req, res) {
+  let response = moviedex;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie => movie.avg_vote >= req.query.avg_vote);
+  }
+
+  res.json(response);
 });
 
 const PORT = 8000;
