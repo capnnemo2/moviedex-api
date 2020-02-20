109c8341d65075dd89fe82b3990f40ad68887743
Ben Hernandez <benjammin234@gmail.com>
2020-02-20 07:54:37 -0800

    Adds cors

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const moviedex = require("./moviedex.json");
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request, punk" });
+  }
+  next();
+});
 
 app.get("/movie", function handleGetMovie(req, res) {
   let response = moviedex;
